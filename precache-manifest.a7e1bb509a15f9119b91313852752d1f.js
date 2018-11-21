ccf51698bed204e4001519fb6e691770ee591f9d
inomdzhon <inomjon92@gmail.com>
2018-11-21 19:56:51 +0300

    Updates

---
+++ precache-manifest.a7e1bb509a15f9119b91313852752d1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75337d66238e4d4051d7",
+    "url": "/gradient-button/static/js/runtime~main.75337d66.js"
+  },
+  {
+    "revision": "138d228053a405e75081",
+    "url": "/gradient-button/static/js/main.138d2280.chunk.js"
+  },
+  {
+    "revision": "5bd83d2e4df51d113714",
+    "url": "/gradient-button/static/js/1.5bd83d2e.chunk.js"
+  },
+  {
+    "revision": "138d228053a405e75081",
+    "url": "/gradient-button/static/css/main.964f83e6.chunk.css"
+  },
+  {
+    "revision": "fadaf1bf69927acd1bc8d2cbe2796a89",
+    "url": "/gradient-button/index.html"
+  }
+];
